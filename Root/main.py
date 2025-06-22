13cf0dd988baf8cd81955d6f83d9dc813f9733f8
KC-decoder <koushani@buffalo.edu>
2025-06-22 18:53:41 -0400

    Initial Commit

---
+++ Root/main.py
@@ -0,0 +1,11 @@
+# def main():
+#     #  scp /Users/koushani/Downloads/RV_images.zip koushani@cbil-lambda.eng.buffalo.edu:/data2/users/koushani/chbmit/Root/data 
+
+
+
+
+
+
+
+# if __name__ == "__main__":
+#     main()
